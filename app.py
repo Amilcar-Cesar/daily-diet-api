4bdcf3dc40dcf3a0b2d7ac18bcc507b85e676f49
Amilcar-Cesar <amilcarcesar0@yahoo.com.br>
2025-08-25 16:45:18 -0300

    criação da API de login

--- app.py
+++ app.py
@@ -1,15 +1,108 @@
 from flask import Flask, jsonify, request
 from database import db
-from models.models import Refeicao
+from models.models import Refeicao, Usuario
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 import datetime
+import bcrypt
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = "your_secret_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:12345@127.0.0.1:3306/Diet_db'
 
+login_manager = LoginManager()
+login_manager.init_app(app)
 db.init_app(app)
+login_manager.login_view = 'login'
 
+@login_manager.user_loader
+def load_user(userd_id):
+    return Usuario.query.get(userd_id)
+
+@app.route('/login', methods=['POST'])
+def login():
+   
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+    
+    if username and password:
+        user = Usuario.query.filter_by(username=username).first()
+        
+        if user and bcrypt.checkpw(str.encode(password), str.encode(user.password)):
+            login_user(user)
+            print(current_user.is_authenticated)
+            return jsonify({"message": "Login realizado com sucesso!"})
+    
+    return jsonify({"message": "Falha no login."}), 401
+    
+@app.route('/logout', methods=['GET'])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message": "Logout realizado com sucesso!"})
+
+
+@app.route('/user/new_login', methods=['POST'])
+def create_login():
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+    role = data.get('role')
+
+    if username and password:
+        hashed_password = bcrypt.hashpw(str.encode(password), bcrypt.gensalt())
+        db.session.add(Usuario(username=username, password=hashed_password, role=role))
+        db.session.commit()
+        return jsonify({"message": "Usuário cadastrado com sucesso!"})
+    return jsonify({"message": "Erro ao cadastrar usuário."}), 500
+
+@login_required
+@app.route('/user/<int:id_user>', methods=['GET'])
+def read_user(id_user):
+    user = Usuario.query.get(id_user)
+
+    if user:
+        return jsonify({
+            'id': user.id,
+            'username': user.username
+        })
+    return jsonify({'message': 'Objeto não encontrado'}), 404
+
+@login_required
+@app.route('/user/update/<int:id_user>', methods=['PUT'])
+def update_user(id_user):
+    data = request.json
+    user = Usuario.query.get(id_user)
+
+    if current_user.id != id_user and current_user.role != 'admin':
+        return jsonify({"message": "Ação não permitida."}), 403
+
+    if user:
+        user.password = data.get("password")
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({"message": "Senha de usuário atualizada com sucesso!"})
+    return jsonify({"message": "Erro ao atualizar senha."}), 500
+
+@login_required
+@app.route('/user/delete/<int:id_user>', methods=['DELETE'])
+def delete_user(id_user):
+    user = Usuario.query.get(id_user)
+
+    if current_user.id != id_user and current_user.role != 'admin':
+        return jsonify({"message": "Ação não permitida."}), 403
+
+    if user:
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify({"message": "Usuário removido com sucesso."})
+    return jsonify({'message': 'Objeto não encontrado'}), 404
+
+#----------------------------------------------------------------
+#-------------------------------- Refeição ------------------------
+#----------------------------------------------------------------
+
+@login_required
 @app.route('/refeição', methods=['POST'])
 def create_refeicao():
     data = request.json
@@ -26,6 +119,7 @@ def create_refeicao():
     return jsonify({"message": "Erro ao cadastrar dados."}), 500
 
 
+@login_required
 @app.route('/refeição/<int:id_ref>', methods=['GET'])
 def read_refeicao(id_ref):
     
@@ -36,6 +130,7 @@ def read_refeicao(id_ref):
     return jsonify({'message': 'Objeto não encontrado'}), 404
 
 
+@login_required
 @app.route('/refeição/lista', methods=['GET'])
 def lista_refeicao():
 
@@ -43,6 +138,7 @@ def lista_refeicao():
     return jsonify([refeicao.to_dict() for refeicao in refeicoes])
 
 
+@login_required
 @app.route('/refeição/editar/<int:id_ref>', methods=['PUT'])
 def update_refeicao(id_ref):
     
@@ -59,6 +155,7 @@ def update_refeicao(id_ref):
     return jsonify({"message": "Erro ao atualizar dados."}), 500
 
 
+@login_required
 @app.route('/refeição/delete/<int:id_ref>', methods=['DELETE'])
 def delete_refeicao(id_ref):
     
--- models/models.py
+++ models/models.py
@@ -1,4 +1,5 @@
 from database import db
+from flask_login import UserMixin
 
 class Refeicao(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
@@ -15,3 +16,9 @@ class Refeicao(db.Model):
             'dataHora': self.dataHora.strftime('%d/%m/%Y %H:%M') if self.dataHora else None,
             'naDieta': self.naDieta
         }
+    
+class Usuario(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    role = db.Column(db.String(50), nullable=False, default='user')
